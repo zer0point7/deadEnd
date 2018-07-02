4321049fe6694d575d6384387bf1af48a9588bab
zer0point7 <30289484+zer0point7@users.noreply.github.com>
2018-07-03 03:04:51 +0530

    Create frequency.cpp

---
+++ frequency.cpp
@@ -0,0 +1,12 @@
+#inlcude<bits/stdc++.h>
+using namespace std;
+int main()
+{
+string st;int ar[26];
+memset(ar,0,sizeof(ar));
+cin>>st;
+for(i=0;i<st.length();i++)
+ar[st[i]-'a']++;
+for(i=0;i<st.length();i++)
+cout<<st[i]<<" "<<ar[st[i]-'a']<<"\n";
+}
